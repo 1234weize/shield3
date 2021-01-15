1ec1607dcd80dbc438bc6dbefd0f9e9cec9bd37c
1234weize <1195517335@qq.com>
2021-01-15 23:16:25 +0800

    modify

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,21 @@
 import React, { Component } from 'react';
+import { stringify } from 'qs';
 import styles from './index.less';
 import ShakaPlayer from './ShakaPlayer';
 import 'shaka-player/dist/controls.css';
+import shaka from 'shaka-player/dist/shaka-player.ui';
 
+const certStr = "MIIE+zCCA+OgAwIBAgIIcRN6MJCE8HEwDQYJKoZIhvcNAQEFBQAwfzELMAkGA1UEBhMCVVMxEzARBgNVBAoMCkFwcGxlIEluYy4xJjAkBgNVBAsMHUFwcGxlIENlcnRpZmljYXRpb24gQXV0aG9yaXR5MTMwMQYDVQQDDCpBcHBsZSBLZXkgU2VydmljZXMgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkwHhcNMjAxMDI1MTA1ODUyWhcNMjEwMTIzMTEwODUyWjB0MQswCQYDVQQGEwJVUzEUMBIGA1UECgwLQXhpbm9tIEdtYkgxEzARBgNVBAsMCjUzVDU1RjdENjYxOjA4BgNVBAMMMUZhaXJQbGF5IFN0cmVhbWluZyBUZXN0OiBBeGlub20gR21iSCAoNTNUNTVGN0Q2NikwgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAJow56XHFClQqYkCD1AGnZPrwojuy38zM32eA6rRdZl9rBnyD2d6JeLW3AqUJAg5l4igAgLtJtZFFuLdqYaKecGnNnTpPms+9jqMrYcNHRPD7ArjWgkQxPFVruHwgibElwpvyUPa3aOBNn3kJzJYguQXgtP2wFNnXVgQPVO3lastAgMBAAGjggIIMIICBDAMBgNVHRMBAf8EAjAAMB8GA1UdIwQYMBaAFGPkR1TLhXFZRiyDrMxEMWRnAyy+MIHqBgNVHSAEgeIwgd8wgdwGCSqGSIb3Y2QFATCBzjCBywYIKwYBBQUHAgIwgb4MgbtJc3N1YW5jZSBvZiB0aGlzIGNlcnRpZmljYXRlIGFuZCByZWxpYW5jZSB0aGVyZW9uIGJ5IGFueSBwYXJ0eSBpcyBzdWJqZWN0IHRvIHRoZSBhcHBsaWNhYmxlIGNlcnRpZmljYXRpb24gcHJhY3RpY2Ugc3RhdGVtZW50IHdoaWNoIGlzIGF2YWlsYWJsZSB1cG9uIHJlcXVlc3QgdG8gcG9saWN5X2F1dGhvcml0eUBhcHBsZS5jb20uMDUGA1UdHwQuMCwwKqAooCaGJGh0dHA6Ly9jcmwuYXBwbGUuY29tL2tleXNlcnZpY2VzLmNybDAdBgNVHQ4EFgQUVhtHxiNQ4Gs7gC+i7VkBgCTcjdwwDgYDVR0PAQH/BAQDAgUgMBQGCyqGSIb3Y2QGDQEIAQH/BAIFADAxBgsqhkiG92NkBg0BAwEB/wQfATMwc3k4MnoyYXF3bDBvaDdocXBhZHhwdXBkYWhuejA3BgsqhkiG92NkBg0BBAEB/wQlAWVtNWVueWRrMnRkbjlka2RrdHhqbXlqd3pzamoxcnJ6Ym9hcDANBgkqhkiG9w0BAQUFAAOCAQEAjJibxPCUDH5jeqabh7HOmkzo0glCAHQaNP2cyYMEFPcCpOsUnWZl711Y6EwE3QhcocXQb7H9PNvxuh0KbeTlYSwvEf/B1nkC3OOtcVAjpUkvdJ7ESiAFS7vDO9RHkQoVW9Sd0hCv8rN2QNSe2F7L/QJPx1sBMP0YqhIM/yxjsGJ2kzYlexDylJoME8OYaO3j/Pyiq7WWEBiH6KzITUZ6XA80ftQErlToE/1izi42TZdAt7ppJjB+gkpzcrzwrbftZtQ4uEjbkEzbZgVAxTIsLBFGs8e9pA9B062A0ao/pAl8/c4eYCyi/DcMjndVq6PLbu6Cq7uS2+j4F7rBwY2UTA=="
+import Ab2Str from 'arraybuffer-to-string';
+import B642Ab from 'base64-to-uint8array';
+
+function arrayToString(array) {
+  var uint16array = new Uint16Array(array.buffer);
+  return String.fromCharCode.apply(null, uint16array);
+}
 class MicroPage extends Component {
   player = React.createRef();
-  widevineLA = 'https://drm-widevine-licensing.axtest.net/AcquireLicense';
-  playreadyLA = 'https://drm-playready-licensing.axtest.net/AcquireLicense';
-  fpsLA = 'https://drm-fairplay-licensing.axtest.net/AcquireLicense';
-
-  state = {
-    mpdUrl: 'https://media.axprod.net/TestVectors/v7-MultiDRM-SingleKey/Manifest_1080p.mpd',
-    entitlementToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjogMSwiY29tX2tleV9pZCI6ICIwYmE0ZmE0Ny01NjUzLTRkMjAtOTZkYy1hY2EyMDA1NmQ5YzIiLCJtZXNzYWdlIjogeyAgInR5cGUiOiAiZW50aXRsZW1lbnRfbWVzc2FnZSIsICAidmVyc2lvbiI6IDIsICAiY29udGVudF9rZXlzX3NvdXJjZSI6IHsgICAgImlubGluZSI6IFsgICAgICB7ICAgICAgICAiaWQiOiAiOWViNDA1MGQtZTQ0Yi00ODAyLTkzMmUtMjdkNzUwODNlMjY2IiAgICAgIH0gICAgXSAgfX19.lds7hxqluHGnU71JKLtTM50gJ8vm7WKXK11AuBnyN9Y',
-  };
-
-  getLAUrl(provider, token) {
-    switch (provider) {
-      case 'widevine':
-        return this.widevineLA + '?AxDrmMessage=' + token;
-      case 'playready':
-        return this.playreadyLA +  '?AxDrmMessage=' + token;
-      case 'fairplay':
-        return this.fpsLA + '?AxDrmMessage=' + token;
-      default:
-        return this.widevineLA + '?AxDrmMessage=' + token;
-    }
-  }
+  path = 'https://shielddemo.xiaogu-tech.com/demo'
 
   componentDidMount = async () => {
     const {
@@ -34,26 +24,80 @@ class MicroPage extends Component {
       videoElement,
     } = this.player.current.getController();
 
-    console.log('player',player);
+    const defaultOptions = {
+      method: 'GET',
+      mode: "cors",
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json; charset=utf-8',
+      }
+    };
+    const params = {
+      fileId: '_Rux4RMT',
+      ips: '',
+      persistent: 0,
+      duration: -1,
+      rtExp: 0,
+      hwSecurity: 0,
+    };
+
+    const newUrl = `${this.path}?${stringify({ ...params })}`;
+    const resp = await fetch(newUrl,defaultOptions);
+    const respData = JSON.parse(await resp.text());
+    const {
+      mpd,
+      hls,
+      spriteWebvtt,
+      drmLA
+    } = respData;
+    console.log(respData)
+    // const req = await fetch(this.fpsCer);
+    // const cert = await req.arrayBuffer();
+    // const serverCertificate = new Uint8Array(cert);
+    // console.log('serverCertificate', serverCertificate)
+    // console.log(B642Ab(certStr))
+
     const support = await this.player.current.getSupport();
     console.log('support',support);
 
     player.configure({
       drm: {
         servers: {
-          'com.widevine.alpha': this.getLAUrl('widevine', this.state.entitlementToken),
-          // 'com.microsoft.playready': this.getLAUrl('playready', this.state.entitlementToken),
-        }
-        // advanced:{
-        //   'com.widevine.alpha': {
-        //     'videoRobustness': 'SW_SECURE_CRYPTO',
-        //     'audioRobustness': 'SW_SECURE_CRYPTO'
-        //   }
-        // }
+          'com.widevine.alpha': drmLA.widevine,
+          'com.microsoft.playready': drmLA.playready,
+          'com.apple.fps.1_0': drmLA.fairplay,
+        },
+        advanced:{
+          'com.apple.fps.1_0':{
+            'serverCertificate': B642Ab(certStr),
+          }
+        },
       }
     });
 
-    await player.load(this.state.mpdUrl);
+
+    player.configure('drm.initDataTransform', (initData, initDataType) => {
+      if (initDataType != 'skd') {
+        return initData;
+      }
+
+      // 'initData' is a buffer containing an 'skd://' URL as a UTF-8 string.
+      const skdUri = shaka.util.StringUtils.fromBytesAutoDetect(initData);
+      // const contentId = getMyContentId(sdkUri);
+      const contentId = arrayToString(initData).replace(/^.*:\/\//, '');
+      console.log(contentId);
+      console.log(skdUri);
+      const cert = player.drmInfo().serverCertificate;
+      return shaka.util.FairPlayUtils.initDataTransform(initData, contentId, cert);
+    });
+
+    if(support.drm['com.apple.fps.1_0']) {
+      await player.load(hls);
+    } else if(support.manifest.mpd){
+      await player.load(mpd);
+    } else {
+      console.log('play failed!')
+    }
     // Trigger play.
     videoElement.play();
   };
